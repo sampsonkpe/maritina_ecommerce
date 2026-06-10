f0474cace13d4c379e9dc070cb9fb00e3ff0908f
Sampson Kojo Kpe <sampsonkpe2@icloud.com>
2026-06-10 16:18:52 +0000

    Feat: update RegisterSerializer to use full_name and adjust permission classes in views

--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -9,13 +9,12 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["id", "email", "password", "first_name", "last_name"]
+        fields = ["id", "email", "password", "full_name"]
 
     def create(self, validated_data):
         user = User.objects.create_user(
             email=validated_data["email"],
             password=validated_data["password"],
-            first_name=validated_data.get("first_name", ""),
-            last_name=validated_data.get("last_name", "")
+            full_name=validated_data.get("full_name", "")
         )
         return user
--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import status
 
 from django.contrib.auth import get_user_model
@@ -14,6 +14,7 @@ User = get_user_model()
 
 
 class RegisterView(APIView):
+    permission_classes = [AllowAny]
 
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
@@ -60,7 +61,7 @@ class MeView(APIView):
         })
 
 class LoginView(APIView):
-
+    permission_classes = [AllowAny]
     def post(self, request):
 
         identifier = request.data.get("identifier")
